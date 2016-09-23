6bb5f49108c917f44638c56ba1412a9314cb9311
Lamin Sanneh <lamin@yaraku.com>
2016-09-23 12:51:49 +0000

    Should be able to add a new todo

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -13,4 +13,16 @@ describe('angular2-end-to-end App', function() {
     let todos = element.all(by.css(".todos .todo"));
     expect(todos.count()).toEqual(3);
   })
+
+  it("Should be able to add a new todo", () => {
+    browser.get("/")
+    let newTodoInput = element(by.css(".add-todo input[type=text]"));
+    newTodoInput.sendKeys("Todo 4");
+
+    let newTodoSubmitButton = element(by.css(".add-todo input[type=submit]"));
+    newTodoSubmitButton.click();
+
+    let todos = element.all(by.css(".todos .todo"));
+    expect(todos.count()).toEqual(4);
+  })
 });
