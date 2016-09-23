e9300dcc2fc6bc5dcd8bd15d2fde4d426acc3d09
Lamin Sanneh <lamin@yaraku.com>
2016-09-23 11:45:41 +0000

    Have three Todos initially

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { Angular2EndToEndPage } from './app.po';
+import { browser, element, by } from 'protractor/globals';
 
 describe('angular2-end-to-end App', function() {
   let page: Angular2EndToEndPage;
@@ -7,8 +8,9 @@ describe('angular2-end-to-end App', function() {
     page = new Angular2EndToEndPage();
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
+  it("should show three todos when we first load the todo app", () => {
+    browser.get("/")
+    let todos = element.all(by.css(".todos .todo"));
+    expect(todos.count()).toEqual(3);
+  })
 });
